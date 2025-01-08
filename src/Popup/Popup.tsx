d77a187e10365ecaadae8ca5e98e9f8dcefa2512
Den-MM <den.eck9@web.de>
2025-01-08 16:56:21 +0100

    Add files via upload

---
+++ src/Popup/Popup.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import First from "./ComponentFolder/First"
+
+function Popup() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id='Popup Visible Area'>
+        <First ></First>
+      </div>
+    </>
+  )
+}
+
+export default Popup
